3672b3a883991d19017ddf020cd83f91f0b4de3b
srtkrwt <36411905+srtkRWT@users.noreply.github.com>
2020-05-01 03:39:07 +0530

    Update diro.js

--- draw/diro.js
+++ draw/diro.js
@@ -7,15 +7,18 @@ window.addEventListener("load", () => {
 	var linesArr = [];
 	
 	let painting = false;
-	
+	let lineColor = "black";
+	let lineWidth = 1;
+	let mouseX = 0;
+	let mouseY = 0;
 	function startPosition(e){
-		console.log(e.button);
-		if(e.button == 2){
-			if(linesArr.length > 0)
-				linesArr.pop();
-		}else if(e.button == 0){
+		if(e.button == 0){
 			painting = true;
-			linesArr.push([]);
+			linesArr.push({
+				lineColor,
+				lineWidth,
+				coords:[]
+			});
 			addSteps(e);
 		}
 	}
@@ -24,22 +27,49 @@ window.addEventListener("load", () => {
 	}
 	
 	function addSteps(e){
+		mouseX = e.x;
+		mouseY = e.y;
 		if(painting == false) return;
-		linesArr[linesArr.length - 1].push({x: e.x, y: e.y});
+		linesArr[linesArr.length - 1].coords.push({x: e.x, y: e.y});
 	}
 	
 	
 	function draw(e){
 		//console.log("drawing");
 		context.lineTo(e.x, e.y);
-		context.strokeStyle = "black";
-		context.stroke();
+			context.stroke();
 		context.moveTo(e.x, e.y);
+	}
+	
+	function commands(e){
+		//console.log(e);
+		if(e.key == 'z'){
+			linesArr.pop();
+		}else if(e.key == '1'){
+			lineWidth = 1;lineColor = "black";
+		}else if(e.key == '2'){
+			lineWidth = 3;lineColor = "black";
+		}else if(e.key == '3'){
+			lineWidth = 5;lineColor = "black";
+		}else if(e.key == '4'){
+			lineColor = "black";
+		}else if(e.key == '5'){
+			lineColor = "red";
+		}else if(e.key == '6'){
+			lineColor = "green";
+		}else if(e.key == '7'){
+			lineColor = "blue";
+		}else if(e.key === '0'){
+			lineColor = "white";
+			lineWidth = 9;
+		}
 	}
 	
 	canvas.addEventListener("mousedown", startPosition);
 	canvas.addEventListener("mousemove", addSteps);
 	canvas.addEventListener("mouseup", endPosition);
+	window.addEventListener("keypress", commands);
+	//window.addEventListener("keyup", commands);
 	
 	
 	function animate(){
@@ -48,12 +78,21 @@ window.addEventListener("load", () => {
 		
 		for(var i = 0; i < linesArr.length; ++i){
 			context.beginPath();
-			for(var j = 0; j < linesArr[i].length; ++j){
-				var e = {x: linesArr[i][j].x, y: linesArr[i][j].y};
+			context.strokeStyle = linesArr[i].lineColor;
+			context.lineWidth = linesArr[i].lineWidth;
+			for(var j = 0; j < linesArr[i].coords.length; ++j){
+				var e = {
+					x: linesArr[i].coords[j].x,
+					y: linesArr[i].coords[j].y,
+				};
 				draw(e);
 			}
 		}
-		
+		context.beginPath();
+		context.strokeStyle = lineColor;
+		context.lineWidth = lineWidth;
+		context.arc(mouseX, mouseY, 1, 0, Math.PI * 2, false);
+		context.stroke();
 	}
 	animate();
 	
@@ -63,4 +102,3 @@ window.addEventListener("resize", () => {
 	canvas.height = window.innerHeight;
 	canvas.width  = window.innerWidth;
 });
-
